de71108ecf657772105964928eca457bd659d375
TXS881 <kalpesh0swain@gmail.com>
2022-09-04 15:21:48 +0530

    all file updated

---
+++ radar.sh
@@ -0,0 +1,28 @@
+export ODATE=$(date +%Y%m%d)
+echo "${ODATE}"
+
+currdir=`pwd`
+echo "${currdir}"
+export log="${currdir}/logs.log"
+
+file_name="/other/splyr.txt"
+
+if [[ -f ${currdir}${file_name} ]]
+then    
+    echo "INFO : splyr file present" >> ${log}
+else
+    echo "error file"               >> ${log}
+    exit 2
+fi
+
+export RS=$?
+
+if [[ ${RS} -eq 0 ]]
+then
+    echo "file run successfully"        >> ${log}
+else
+    echo "file not found"               >> ${log}
+    exit 3
+fi
+
+exit 0
